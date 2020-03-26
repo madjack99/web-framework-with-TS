98e65572dfa0de6120948416449875b6a0559e95
Alex <kavanokami@gmail.com>
2020-03-26 15:52:23 +0400

    feat: add trigger method

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myName', age: 20 });
 
-user.on('change', () => {});
-user.on('change', () => {});
+user.on('change', () => {
+  console.log('change 1');
+});
+user.on('change', () => {
+  console.log('change 2');
+});
+user.trigger('chadange');
 console.log(user);
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,10 +3,10 @@ interface UserProps {
   age?: number;
 }
 
-type CallBack = () => void;
+type Callback = () => void;
 
 export class User {
-  events: { [key: string]: CallBack[] } = {};
+  events: { [key: string]: Callback[] } = {};
 
   constructor(private data: UserProps) {}
 
@@ -18,9 +18,19 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  on(eventName: string, callBack: CallBack): void {
+  on(eventName: string, callback: Callback): void {
     const handlers = this.events[eventName] || [];
-    handlers.push(callBack);
+    handlers.push(callback);
     this.events[eventName] = handlers; // ???
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(callback => callback());
   }
 }
