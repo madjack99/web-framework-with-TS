1e7ceb0d380adceddea54bc53a95009c009131ee
Alex <kavanokami@gmail.com>
2020-03-27 17:14:52 +0400

    feat: make new Sync class

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ const user = new User({ id: 3 });
 user.set({ name: 'Bob', age: 22 });
 user.save();
 console.log(user.get('id'));
+
+user.events.on('change', () => console.log('change'));
+user.events.trigger('change');
