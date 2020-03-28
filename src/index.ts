5c3696a51d3461881c98154fc2ed21d75872eba3
Alex <kavanokami@gmail.com>
2020-03-28 13:54:17 +0400

    feat: change methods to => functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Jack', age: 55 });
+
+console.log(user.get('name'));
+user.on('change', () => console.log('user was change'));
+user.trigger('change');
