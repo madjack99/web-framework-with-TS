220286c4870ab5a7733633c472840a43a48c8ee9
Alex <kavanokami@gmail.com>
2020-03-28 14:14:06 +0400

    feat: add 'set' method to User class

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Jack', age: 55 });
 
-console.log(user.get('name'));
 user.on('change', () => console.log('user was change'));
-user.trigger('change');
+user.set({ name: 'Bill' });
