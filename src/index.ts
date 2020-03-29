38d6ae6355b6c37081a3dd81e71615af116a4066
Alex <kavanokami@gmail.com>
2020-03-29 12:53:33 +0400

    feat: add eventsMap method to UserForm

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import { Collection } from './models/Collection';
-import { User, UserProps } from './models/User';
+import { UserForm } from './views/UserForm';
 
-const collection = User.buildUserCollection();
-collection.on('change', () => console.log(collection));
-collection.fetch();
+const form = new UserForm(document.getElementById('root'));
+form.render();
