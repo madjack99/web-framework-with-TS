b1035812d7c2e6e1b286f435a4c1e3b1967ca434
Alex <kavanokami@gmail.com>
2020-03-29 13:52:16 +0400

    feat: display user info and set random age

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const form = new UserForm(document.getElementById('root'));
+const user = User.buildUser({ name: 'Name', age: 20 });
+
+const form = new UserForm(document.getElementById('root'), user);
 form.render();
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,26 +1,35 @@
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onHeaderHover
+      'click:.set-age': this.onSetAgeClick
     };
   }
 
-  onHeaderHover() {
-    console.log('h1 was hovered over');
-  }
-
-  onButtonClick(): void {
-    console.log('button clicked');
-  }
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
   template(): string {
     return `
       <div>
           <h1>User Form</h1>
-          <input /><button>Button</button>
+          <div>User name: ${this.model.get('name')}</div>
+          <div>User age: ${this.model.get('age')}</div>
+          <input />
+          <button>Button</button>
+          <button class='set-age'>Set random age</button>
       </di>
       `;
   }
@@ -38,6 +47,8 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = '';
+
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
