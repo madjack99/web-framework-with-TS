076fc12d158a03ff868d7d6511f35a917f88bc56
Alex <kavanokami@gmail.com>
2020-03-29 17:04:47 +0400

    feat: add regionsMap methods to view classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'Name', age: 20 });
@@ -6,8 +6,9 @@ const user = User.buildUser({ name: 'Name', age: 20 });
 const root = document.getElementById('root');
 
 if (root) {
-  const form = new UserForm(root, user);
-  form.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  console.log(userEdit);
 } else {
   throw new Error('Element not found');
 }
